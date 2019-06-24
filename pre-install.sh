cd29f15be9d97290ca4817de6723963f9c569f7b
DigiDaz <DigiDaz@users.noreply.github.com>
2019-06-24 22:58:11 +0200

    Update pre-install.sh

--- pre-install.sh
+++ pre-install.sh
@@ -1,2 +1,4 @@
 sed -i 's/.*files.freeswitch.org\/repo\/deb\/freeswitch-1.8\/fsstretch-archive-keyring.gpg.*/                wget -qO - http:\/\/freeswitch.sip247.com\/debian\/freeswitch-stable-amd64\/fs-stretch.gpg.key | apt-key add -/' /usr/src/fusionpbx-install.sh/debian/resources/switch/package-release.sh
 sed -i 's/.*files.freeswitch.org\/repo\/deb\/freeswitch-1.8\/ stretch.*/                echo "deb http:\/\/freeswitch.sip247.com\/debian\/freeswitch-stable-amd64\/ stretch main" > \/etc\/apt\/sources.list.d\/freeswitch.list/' /usr/src/fusionpbx-install.sh/debian/resources/switch/package-release.sh
+sed -i 's/switch_source=true/switch_source=false/' /usr/src/fusionpbx-install.sh/debian/resources/config.sh
+sed -i 's/switch_package=false/switch_package=true/' /usr/src/fusionpbx-install.sh/debian/resources/config.sh
